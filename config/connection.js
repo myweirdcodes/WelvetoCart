75a392f09293d9197eaf78e14981b56e9fda248d
Sachin Raj <sachintk99@gmail.com>
2023-01-21 18:01:02 +0530

    commmit after modifying db connectin to atlas

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,24 @@
 const mongoose  = require("mongoose");
-// const mongoose_connect = process.env.MONGODB_CONNECTION_ID
+const mongoose_connect = process.env.MONGODB_CONNECTION_ID
 
-mongoose.connect("mongodb://localhost:27017/WelvetoDB",{
+// db connection with database in localStorage
+
+// mongoose.connect("mongodb://localhost:27017/WelvetoDB",{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// },(err)=>{
+//     if(err){
+//         console.log('connection err'+err)
+//     }
+//     else{
+//         console.log('connection established')
+//     }
+// });
+
+
+// db connectin with database in atlas cloud storage
+
+mongoose.connect(mongoose_connect,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 },(err)=>{
